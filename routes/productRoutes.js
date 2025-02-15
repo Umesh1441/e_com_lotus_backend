2ba63a60c8a5ba7a3c03c1f3197b94143230363d
Aman <Palaman2626@gmail.com>
2025-02-15 13:43:06 +0530

    product functionality added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,17 @@ const Product = require('../models/Product');
 
 const router = express.Router();
 
+router.post('/add', async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        const product = new Product({ name, price });
+        await product.save();
+        res.status(201).json(product);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
 // Get all products
 router.get('/', async (req, res) => {
     try {
